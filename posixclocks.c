9cd4c84ffc546f94ae58b4ae87b31150cfe98c70
Adrian Guenter <adrianguenter@gmail.com>
2014-08-20 06:00:01 -0400

    Fix empty value

--- posixclocks.c
+++ posixclocks.c
@@ -89,7 +89,7 @@ PHP_MINFO_FUNCTION(posixclocks)
   strcpy(isSupported, "Yes");
   clock_getres(CLOCK_MONOTONIC, &res);
   dResult = res.tv_sec + res.tv_nsec / 1000000000.0;
-  snprintf(precision, 50, "%f", dResult);
+  snprintf(precision, 50, "%e", dResult);
   #else
   strcpy(isSupported, "No");
   #endif
