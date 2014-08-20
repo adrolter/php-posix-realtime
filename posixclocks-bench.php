770f51eab62fa867214261733f2bc4cc671eece9
Adrian Guenter <adrianguenter@gmail.com>
2014-08-19 23:15:04 -0400

    Replace tab chars with 2 spaces

--- posixclocks-bench.php
+++ posixclocks-bench.php
@@ -1,19 +1,19 @@
 <?php
 /*
-	Copyright 2011 Adrian Guenter	<adrianguenter@gmail.com>
-	
-	This file is part of php_posixclocks and licensed
-	under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
-		
-		http://www.apache.org/licenses/LICENSE-2.0
-		
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
+  Copyright 2011 Adrian Guenter  <adrianguenter@gmail.com>
+
+  This file is part of php_posixclocks and licensed
+  under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 */
 
 echo "\n     PHP_POSIXCLOCKS BENCHMARK    \n";
@@ -42,12 +42,12 @@ echo " microtime:\t" . $mtTotal . "\n";
 echo "---------------------------------\n";
 
 if ($gtTotal < $mtTotal) {
-	$pcnt = round((($mtTotal / $gtTotal) * 100) - 100, 3);
-	echo " posix_clock_gettime was {$pcnt}% faster\n";
+  $pcnt = round((($mtTotal / $gtTotal) * 100) - 100, 3);
+  echo " posix_clock_gettime was {$pcnt}% faster\n";
 }
 else {
-	$pcnt = round((($gtTotal / $mtTotal) * 100) - 100, 3);
-	echo " microtime was {$pcnt}% faster\n";
+  $pcnt = round((($gtTotal / $mtTotal) * 100) - 100, 3);
+  echo " microtime was {$pcnt}% faster\n";
 }
 
 echo "---------------------------------\n";
--- posixclocks.c
+++ posixclocks.c
@@ -1,18 +1,18 @@
 /*
-	Copyright 2014 Adrian Guenter	<adrianguenter@gmail.com>
-	
-	This file is part of php_posixclocks and licensed
-	under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
-		
-		http://www.apache.org/licenses/LICENSE-2.0
-		
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
+  Copyright 2014 Adrian Guenter  <adrianguenter@gmail.com>
+
+  This file is part of php_posixclocks and licensed
+  under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 */
  
 #ifdef HAVE_CONFIG_H
@@ -30,24 +30,24 @@ static int le_posixclocks;
 
 PHP_MINIT_FUNCTION(posixclocks)
 {
-	#ifdef CLOCK_REALTIME
+  #ifdef CLOCK_REALTIME
     REGISTER_LONG_CONSTANT("POSIX_CLOCK_REALTIME", CLOCK_REALTIME, CONST_CS | CONST_PERSISTENT);
   #endif
 
   #ifdef CLOCK_MONOTONIC
-	  REGISTER_LONG_CONSTANT("POSIX_CLOCK_MONOTONIC", CLOCK_MONOTONIC, CONST_CS | CONST_PERSISTENT);
-	#endif
+    REGISTER_LONG_CONSTANT("POSIX_CLOCK_MONOTONIC", CLOCK_MONOTONIC, CONST_CS | CONST_PERSISTENT);
+  #endif
 
-	#ifdef CLOCK_PROCESS_CPUTIME_ID
-	  REGISTER_LONG_CONSTANT("POSIX_CLOCK_PROCESS_CPUTIME_ID", CLOCK_PROCESS_CPUTIME_ID, CONST_CS | CONST_PERSISTENT);
-	#endif
+  #ifdef CLOCK_PROCESS_CPUTIME_ID
+    REGISTER_LONG_CONSTANT("POSIX_CLOCK_PROCESS_CPUTIME_ID", CLOCK_PROCESS_CPUTIME_ID, CONST_CS | CONST_PERSISTENT);
+  #endif
 
-	#ifdef CLOCK_THREAD_CPUTIME_ID
-	  REGISTER_LONG_CONSTANT("POSIX_CLOCK_THREAD_CPUTIME_ID", CLOCK_THREAD_CPUTIME_ID, CONST_CS | CONST_PERSISTENT);
-	#endif
+  #ifdef CLOCK_THREAD_CPUTIME_ID
+    REGISTER_LONG_CONSTANT("POSIX_CLOCK_THREAD_CPUTIME_ID", CLOCK_THREAD_CPUTIME_ID, CONST_CS | CONST_PERSISTENT);
+  #endif
 
-	#ifdef CLOCK_MONOTONIC_RAW
-  	  REGISTER_LONG_CONSTANT("POSIX_CLOCK_MONOTONIC_RAW", CLOCK_MONOTONIC_RAW, CONST_CS | CONST_PERSISTENT);
+  #ifdef CLOCK_MONOTONIC_RAW
+      REGISTER_LONG_CONSTANT("POSIX_CLOCK_MONOTONIC_RAW", CLOCK_MONOTONIC_RAW, CONST_CS | CONST_PERSISTENT);
   #endif
 
   #ifdef CLOCK_REALTIME_COARSE
@@ -62,102 +62,102 @@ PHP_MINIT_FUNCTION(posixclocks)
     REGISTER_LONG_CONSTANT("POSIX_CLOCK_BOOTTIME", CLOCK_BOOTTIME, CONST_CS | CONST_PERSISTENT);
   #endif
 
-	return SUCCESS;
+  return SUCCESS;
 }
 
 
 PHP_MSHUTDOWN_FUNCTION(posixclocks)
 {
-	return SUCCESS;
+  return SUCCESS;
 }
 
 
 PHP_MINFO_FUNCTION(posixclocks)
 {
-	php_info_print_table_start();
-	php_info_print_table_header(2, "posixclocks support", "enabled");
-	php_info_print_table_end();
+  php_info_print_table_start();
+  php_info_print_table_header(2, "posixclocks support", "enabled");
+  php_info_print_table_end();
 }
 
 
 PHP_FUNCTION(posix_clock_gettime)
 {
-	long clkId = 0;
-	struct timespec currTime;
-	double dResult;
-	
-	if (ZEND_NUM_ARGS() > 1) {
-		WRONG_PARAM_COUNT;
-	}
-	
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "|l", &clkId) != SUCCESS) {
-		return;
-	}
-	
-	if (clock_gettime(clkId, &currTime) == -1) {
-		if (errno == EINVAL) {
-			php_error_docref(NULL TSRMLS_CC, E_ERROR, "The POSIX clock ID '%ld' is not supported on this system", clkId);
-			return;
-		}
-		else if (errno == EFAULT) {
-			return;
-		}
-	}
-	
-	dResult = currTime.tv_sec + currTime.tv_nsec / 1000000000.0;
-	
-	RETURN_DOUBLE(dResult);
+  long clkId = 0;
+  struct timespec currTime;
+  double dResult;
+
+  if (ZEND_NUM_ARGS() > 1) {
+    WRONG_PARAM_COUNT;
+  }
+
+  if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "|l", &clkId) != SUCCESS) {
+    return;
+  }
+
+  if (clock_gettime(clkId, &currTime) == -1) {
+    if (errno == EINVAL) {
+      php_error_docref(NULL TSRMLS_CC, E_ERROR, "The POSIX clock ID '%ld' is not supported on this system", clkId);
+      return;
+    }
+    else if (errno == EFAULT) {
+      return;
+    }
+  }
+
+  dResult = currTime.tv_sec + currTime.tv_nsec / 1000000000.0;
+
+  RETURN_DOUBLE(dResult);
 }
 
 
 PHP_FUNCTION(posix_clock_getres)
 {
-	long clkId = 0;
-	struct timespec res;
-	double dResult;
-	
-	if (ZEND_NUM_ARGS() > 1) {
-		WRONG_PARAM_COUNT;
-	}
-	
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "|l", &clkId) != SUCCESS) {
-		return;
-	}
-	
-	if (clock_getres(clkId, &res) == -1) {
-		if (errno == EINVAL) {
-			php_error_docref(NULL TSRMLS_CC, E_ERROR, "The POSIX clock ID '%ld' is not supported on this system", clkId);
-			return;
-		}
-		else if (errno == EFAULT) {
-			return;
-		}
-	}
-	
-	dResult = res.tv_sec + res.tv_nsec / 1000000000.0;
-	
-	RETURN_DOUBLE(dResult);
+  long clkId = 0;
+  struct timespec res;
+  double dResult;
+
+  if (ZEND_NUM_ARGS() > 1) {
+    WRONG_PARAM_COUNT;
+  }
+
+  if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "|l", &clkId) != SUCCESS) {
+    return;
+  }
+
+  if (clock_getres(clkId, &res) == -1) {
+    if (errno == EINVAL) {
+      php_error_docref(NULL TSRMLS_CC, E_ERROR, "The POSIX clock ID '%ld' is not supported on this system", clkId);
+      return;
+    }
+    else if (errno == EFAULT) {
+      return;
+    }
+  }
+
+  dResult = res.tv_sec + res.tv_nsec / 1000000000.0;
+
+  RETURN_DOUBLE(dResult);
 }
 
 
 const zend_function_entry posixclocks_functions[] = {
-	PHP_FE(posix_clock_gettime,	NULL)
-	PHP_FE(posix_clock_getres,	NULL)
-	PHP_FE_END
+  PHP_FE(posix_clock_gettime,  NULL)
+  PHP_FE(posix_clock_getres,  NULL)
+  PHP_FE_END
 };
 
 
 zend_module_entry posixclocks_module_entry = {
-	STANDARD_MODULE_HEADER,
-	"posixclocks",
-	posixclocks_functions,
-	PHP_MINIT(posixclocks),
-	PHP_MSHUTDOWN(posixclocks),
-	NULL,		/* PHP_RINIT */
-	NULL,	/* PHP_RSHUTDOWN */
-	PHP_MINFO(posixclocks),
-	PHP_POSIXCLOCKS_VERSION,
-	STANDARD_MODULE_PROPERTIES
+  STANDARD_MODULE_HEADER,
+  "posixclocks",
+  posixclocks_functions,
+  PHP_MINIT(posixclocks),
+  PHP_MSHUTDOWN(posixclocks),
+  NULL,    /* PHP_RINIT */
+  NULL,  /* PHP_RSHUTDOWN */
+  PHP_MINFO(posixclocks),
+  PHP_POSIXCLOCKS_VERSION,
+  STANDARD_MODULE_PROPERTIES
 };
 
 
