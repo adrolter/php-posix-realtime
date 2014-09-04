17817aff4dec8bdd4de951b656ef2befc2e63d83
Adrian Guenter <adrianguenter@gmail.com>
2014-09-04 15:25:31 -0400

    Update stubs with new API

--- stubs/posixclocks.php
+++ stubs/posixclocks.php
@@ -5,52 +5,52 @@
  * is affected by discontinuous jumps in the system time (e.g., manual clock
  * updates, NTP clock updates, etc).
  */
-define('PSXCLK_CLOCK_REALTIME', 0);
+define('PSXCLK_C_REALTIME', 0);
 
 /**
  * Clock that cannot be set and represents monotonic time since some
  * unspecified starting point. This clock is not affected by discontinuous
  * jumps in the system time.
  */
-define('PSXCLK_CLOCK_MONOTONIC', 1);
+define('PSXCLK_C_MONOTONIC', 1);
 
 /**
  * Per-process CPU-time clock (measures CPU time consumed by all threads in
  * the process).
  */
-define('PSXCLK_CLOCK_PROCESS_CPUTIME_ID', 2);
+define('PSXCLK_C_PROCESS_CPUTIME_ID', 2);
 
 
 /**
  * Thread-specific CPU-time clock.
  */
-define('PSXCLK_CLOCK_THREAD_CPUTIME_ID', 3);
+define('PSXCLK_C_THREAD_CPUTIME_ID', 3);
 
 /**
  * (Linux >= 2.6.28) Similar to CLOCK_MONOTONIC, but provides access to a raw
  * hardware-based time that is not subject to NTP adjustments or the
  * incremental adjustments performed by adjtime.
  */
-define('PSXCLK_CLOCK_MONOTONIC_RAW', 4);
+define('PSXCLK_C_MONOTONIC_RAW', 4);
 
 /**
  * (Linux >= 2.6.32) A faster but less precise version of CLOCK_REALTIME.
  * Use when you need very fast, but not fine-grained timestamps.
  */
-define('PSXCLK_CLOCK_REALTIME_COARSE', 5);
+define('PSXCLK_C_REALTIME_COARSE', 5);
 
 /**
  * (Linux >= 2.6.32) A faster but less precise version of CLOCK_MONOTONIC.
  * Use when you need very fast, but not fine-grained timestamps.
  */
-define('PSXCLK_CLOCK_MONOTONIC_COARSE', 6);
+define('PSXCLK_C_MONOTONIC_COARSE', 6);
 
 /**
  * (Linux >= 2.6.39) Identical to CLOCK_MONOTONIC, except it also includes
  * any time that the system is suspended. This allows applications to get
  * a suspend-aware monotonic clock.
  */
-define ('PSXCLK_CLOCK_BOOTTIME', 7);
+define ('PSXCLK_C_BOOTTIME', 7);
 
 /**
  * Determines whether or not the specified clock is supported on the system.
@@ -62,13 +62,18 @@ function posix_clock_available($clock_id) {}
 /**
  * Retrieves the value (time) of the specified clock.
  * @param int $clock_id
- * @return float|string
+ * @param int $return_as
+ * @return string|float|stdClass
  */
-function posix_clock_gettime($clock_id = PSXCLK_CLOCK_REALTIME) {}
+function posix_clock_gettime(
+	$clock_id = PSXCLK_C_REALTIME,
+	$return_as = PSXCLK_AS_STRING,
+	$floor_to = false
+) {}
 
 /**
  * Retrieves the resolution (precision) of the specified clock.
  * @param int $clock_id
- * @return float
+ * @return int
  */
-function posix_clock_getres($clock_id = PSXCLK_CLOCK_REALTIME) {}
+function posix_clock_getres($clock_id = PSXCLK_C_REALTIME) {}
