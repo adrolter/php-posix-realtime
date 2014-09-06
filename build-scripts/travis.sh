1a209e5eaf05d801dfaf37f3b60670dbc2a5b9be
Adrian Guenter <adrianguenter@gmail.com>
2014-09-06 06:26:59 -0400

    Update Travis build script

--- build-scripts/travis.sh
+++ build-scripts/travis.sh
@@ -2,9 +2,24 @@
 
 phpize
 
+if [ "$?" != "0" ]; then
+	echo "Phpize failed."
+	exit 1
+fi
+
 ./configure
 
+if [ "$?" != "0" ]; then
+	echo "Configure failed."
+	exit 1
+fi
+
 make
+
+if [ "$?" != "0" ]; then
+	echo "Make failed."
+	exit 1
+fi
 
 PSXCLK_MAKE_TEST=$(echo "n" | make test) 
 
