e6200ef9aa34197567e553bced59a282fec087da
Adrian Guenter <adrianguenter@gmail.com>
2014-09-06 21:03:30 -0400

    Rework Travis main build script

--- build-scripts/travis/after_failure.sh
+++ build-scripts/travis/after_failure.sh
@@ -4,6 +4,6 @@ printf '%s' "${PSXRT_MAKE_TEST}"
 
 printf '\n'
 
-find tests/ -name "*.diff" | while read diffFile do; do
-	printf '\n%s << EOF\n%s\nEOF\n' "$diffFile" "$(cat $diffFile)"
-done
+#find tests/ -name "*.diff" | while read diffFile do; do
+#	printf '\n%s << EOF\n%s\nEOF\n' "$diffFile" "$(cat $diffFile)"
+#done
--- build-scripts/travis/main.sh
+++ build-scripts/travis/main.sh
@@ -1,26 +1,51 @@
 #!/usr/bin/env sh
 
+header () {
+	printf '\n\n\n\n*************************************\n\t%s\n*************************************\n' "$1"
+}
+
+
+header "PHPIZE"
+
 phpize
 
 if [ "$?" != "0" ]; then
-	printf 'Phpize failed.'
+	printf '\n\nPhpize failed.\n\n'
 	exit 1
 fi
+
+
+header "CONFIGURE"
 
 ./configure
 
 if [ "$?" != "0" ]; then
-	printf 'Configure failed.'
+	printf '\n\nConfigure failed.\n\n'
 	exit 1
 fi
+
+header "MAKE"
 
 make
 
 if [ "$?" != "0" ]; then
-	printf 'Make failed.'
+	printf '\n\nMake failed.\n\n'
 	exit 1
 fi
 
-PSXRT_MAKE_TEST=$(echo "n" | make test)
 
-printf '%s' "${PSXRT_MAKE_TEST}" | grep "Tests passed.*(100.0%)$"
+header "TEST"
+
+# Store the result of make test
+PSXRT_MAKE_TEST=$(TEST_PHP_ARGS="-q --show-diff" make test)
+
+# Print "make test" output
+printf '%s' "${PSXRT_MAKE_TEST}"
+
+# grep returns non-0 exit code if not matched
+printf '%s' "${PSXRT_MAKE_TEST}" | grep -q "Tests passed.*(100.0%)$"
+
+if [ "$?" != "0" ]; then
+	printf '\n\nTest(s) failed.\n\n'
+	exit 1
+fi
